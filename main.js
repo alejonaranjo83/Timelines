aaf4c699bb64b3ec5a1a5b2ff1354c516a031a70
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-02-17 18:56:13 -0500

    Starting to set how the screens will hide

---
+++ main.js
@@ -0,0 +1,12 @@
+// alert("Checking that JS is working");
+
+const cover = document.querySelector(".cover");
+const mainTL = document.querySelector(".main-grid");
+const mainTLaside = document.querySelector(".aside-grid")
+const personalTL = document.querySelector(".personal");
+const future = document.querySelector("future-think");
+
+
+// mainTL.style.display = "none"
+
+// mainTLaside.style.display = "none"

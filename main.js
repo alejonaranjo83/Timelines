62ec9593ee82ffa44df5e82abe814f56c5bd686c
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-02-19 15:23:36 -0500

    creating class for milestone with ROR

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ const future = document.querySelector("future-think");
 // mainTL.style.display = "none"
 
 // mainTLaside.style.display = "none"
+
+
+
+// Creating class instance with ROR:
+class Milestone {
+    constructor({
+        name,
+        year,
+        percentage,
+        descrip,
+        image,
+    }) {
+        this.name = name;
+        this.year = year;
+        this.percentage = percentage;
+        this.descrip = descrip;
+        this.image = image;
+    }
+}

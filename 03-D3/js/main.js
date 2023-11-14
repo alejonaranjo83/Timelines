3ec39f9159759bf2f52507c6d153d46d0017a776
alejonaranjo83-linux <alejonaranjo83@gmail.com>
2023-11-13 19:47:20 -0500

    Reading data for the first timeline and converting the numeric values to Number format in JS

--- 03-D3/js/main.js
+++ 03-D3/js/main.js
@@ -1,1 +1,27 @@
-// alert("Js is working")
+alert("Js is working")
+
+const svg = d3.select("#d3-tl1").append("svg")
+    .attr("width", "100%") //dimensions will be restricted to what was defined with CSS grid
+    .attr("height", "100%")
+
+d3.dsv(";", "data/tl1.csv").then(data => { //because the data is not separated with "," but with ";" it will be used the "dsv" format
+    data.forEach(d => {
+        const timeValue = Number(d.Time.replace(',', '.')); //converting the decimal separator from ',' to '.'
+
+        // Converting the values from the "Time" column
+        if (!isNaN(timeValue)) {
+            d.Time = timeValue;
+        } else {
+            console.warn("Unable to convert ${d.Time} to a number")
+        }
+
+        // Converting the values from the "Percentage" column
+        const timePercent = Number(d.Percentage.replace(',', '.'));
+        if (!isNaN(timePercent)) {
+            d.Percent = timePercent;
+        } else {
+            console.warn("Unable to convert ${d.Percent} to a number")
+        }
+    })
+    console.log(data)
+})
